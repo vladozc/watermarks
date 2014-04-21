91c5c44159e7dd658963dc33267a69795b1bf08f
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-04-21 19:42:45 +0200

    visible wm writer

--- src/watermarks/core/writers/visible.py
+++ src/watermarks/core/writers/visible.py
@@ -10,18 +10,17 @@ logger = logging.getLogger()
 
 
 def init(args):
-    '''Returns initialized Lsb (writer) object from arguments passed from
+    '''Returns initialized Visible (writer) object from arguments passed from
     command line.
     '''
     wm = create_watermark(args.watermark)
-    return Lsb(args.sources, args.dest_dir, args.format, wm)
+    return Visible(args.sources, args.dest_dir, args.format, wm)
 
 
-class Lsb(object):
-    '''Lsb (least significant bit) is method that changes least significant
-    (last) bit for every subpixel in image according to reference image.
-    With this approach you can generate new image almost identical to
-    original image but with your hidden watermark.
+class Visible(object):
+    '''Visible method inserts visible watermark to top left corner of
+    original image.
+
     '''
     allowed_modes = ('CMYK', 'L', 'RGB')
 
@@ -86,39 +85,16 @@ class Lsb(object):
         if src_img.mode in self.allowed_modes:
             base_name, _ = os.path.splitext(os.path.basename(filepath))
             logger.info('Processing file "%s"', filepath)
-            src_img.load()
-            bands = src_img.split()
+            #src_img.load()
             dst_filepath = os.path.join(self.destination, '%s%s.%s' % (base_name, self.suffix, self.format))
-            bands_wm = []
-            for band in bands:
-                band_wm = Image.new('L', src_img.size)
-                band_wm.putdata([convert(orig_px, wm_px, self.wm.threshold) 
-                                 for orig_px, wm_px 
-                                 in zip(band.getdata(), self.wm.band.getdata())
-                                ])
-                bands_wm.append(band_wm)
-            dst_img = Image.merge(src_img.mode, bands_wm)
+            dst_img = self._create_watermarked(src_img)
             dst_img.save(dst_filepath)
             logger.info('Generated file "%s".', dst_filepath)
             return dst_filepath
         else:
             logger.warning('File "%s" is in unsupported mode "%s". (skip)', filepath, src_img.mode)
 
-
-def convert(orig_px, wm_px, threshold):
-    '''Returns modified (last bit) value for subpixel.
-
-    :param int orig_px:
-        Current image subpixel value.
-    :param int wm_px:
-        Watermark subpixel value.
-    :param int threshold:
-        If `wm_px` is less or equal than this value, `orig_px` will
-        change it's last bit value to 0 and to 1 if greater.
-    :return:
-        New subpixel value.
-    :rtype: int
-    '''
-    if wm_px <= threshold:
-        return orig_px & 254
-    return orig_px | 1
+    def _create_watermarked(self, src_img):
+        dst_img = src_img.copy()
+        dst_img.paste(self.wm.img, (0, 0))
+        return dst_img
