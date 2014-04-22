f6590ea6e3e92c0d4f5c541dbdb77f884640ec09
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-04-22 20:57:55 +0200

    selftest

--- src/watermarks/__init__.py
+++ src/watermarks/__init__.py
@@ -1,1 +1,42 @@
+import os
+
+from PIL import Image
+
+
 __version__ = '0.0.1'
+
+
+def selftest(tmp='/tmp'):
+    filepath = os.path.join(tmp, 'watermarks_test.png')
+    _clean(filepath)
+    try:
+        _do_test(filepath)
+        _clean(filepath)
+        return True
+    except Exception, edata:
+        print 'Error: %s' % edata
+        return False
+
+
+def _do_test(filepath):
+    sample_data = [1, 2, 3, 4]
+    band = Image.new('L', (2, 2))
+    band.putdata(sample_data)
+
+    img = Image.merge('L', [band])
+    img.save(filepath)
+
+    if not os.path.exists(filepath):
+        raise Exception('file was not created')
+
+    img = Image.open(filepath)
+    img.load()
+    img_data = list(img.split()[0].getdata())
+
+    if sample_data != img_data:
+        raise Exception('image has invalid data')
+
+    
+def _clean(filepath):
+    if os.path.exists(filepath):
+        os.unlink(filepath)
