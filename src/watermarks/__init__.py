eacfe1d6210ba023e88b27cabda52bc5d9ef8c16
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-04-25 19:29:23 +0200

    move selftest out from toplevel package

--- src/watermarks/__init__.py
+++ src/watermarks/__init__.py
@@ -4,45 +4,4 @@ https://github.com/vladozc/watermarks
 https://pypi.python.org/pypi/watermarks
 
 '''
-import os
-
-from PIL import Image
-
-
 __version__ = '0.1.1-devel'
-
-
-def selftest(tmp='/tmp'):
-    filepath = os.path.join(tmp, 'watermarks_test.png')
-    _clean(filepath)
-    try:
-        _do_test(filepath)
-        _clean(filepath)
-        return True
-    except Exception as edata:
-        print('Error: %s' % edata)
-        return False
-
-
-def _do_test(filepath):
-    sample_data = [1, 2, 3, 4]
-    band = Image.new('L', (2, 2))
-    band.putdata(sample_data)
-
-    img = Image.merge('L', [band])
-    img.save(filepath)
-
-    if not os.path.exists(filepath):
-        raise Exception('file was not created')
-
-    img = Image.open(filepath)
-    img.load()
-    img_data = list(img.split()[0].getdata())
-
-    if sample_data != img_data:
-        raise Exception('image has invalid data')
-
-    
-def _clean(filepath):
-    if os.path.exists(filepath):
-        os.unlink(filepath)
---
+++ src/watermarks/selftest.py
@@ -0,0 +1,39 @@
+import os
+
+from PIL import Image
+
+
+def selftest(tmp='/tmp'):
+    filepath = os.path.join(tmp, 'watermarks_test.png')
+    _clean(filepath)
+    try:
+        _do_test(filepath)
+        _clean(filepath)
+        return True
+    except Exception as edata:
+        print('Error: %s' % edata)
+        return False
+
+
+def _do_test(filepath):
+    sample_data = [1, 2, 3, 4]
+    band = Image.new('L', (2, 2))
+    band.putdata(sample_data)
+
+    img = Image.merge('L', [band])
+    img.save(filepath)
+
+    if not os.path.exists(filepath):
+        raise Exception('file was not created')
+
+    img = Image.open(filepath)
+    img.load()
+    img_data = list(img.split()[0].getdata())
+
+    if sample_data != img_data:
+        raise Exception('image has invalid data')
+
+
+def _clean(filepath):
+    if os.path.exists(filepath):
+        os.unlink(filepath)
