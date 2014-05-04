652faa66afdca8cbe187cafde17b5e35b0099d4e
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-05-04 11:37:15 +0200

    format logger

--- src/watermarks/core/__init__.py
+++ src/watermarks/core/__init__.py
@@ -6,4 +6,6 @@ def setup_logger(log_level=None):
     logger = logging.getLogger()
     logger.setLevel(log_level)
     stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
