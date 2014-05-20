0a18044ea9e16a94a57d5fff208a12dcbc4dbf95
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-05-20 22:28:08 +0200

    test for suffix arg in reader

--- test/functiontests/lsb/test_reader.py
+++ test/functiontests/lsb/test_reader.py
@@ -98,13 +98,14 @@ def test_dir(dst_dir):
 @in_tmp
 def test_bin(dst_dir):
     filepath = os.path.join(DATA_DIR, 'gen-%s-g.png' % IM_PREFIX)
-    generated_filepath = os.path.join(dst_dir, 'gen-%s-g_L.png' % IM_PREFIX)
+    suffix = '_test'
+    generated_filepath = os.path.join(dst_dir, 'gen-%s-g_L%s.png' % (IM_PREFIX, suffix))
     if os.path.exists(generated_filepath):
         os.unlink(generated_filepath)
 
     try:
         sp = subprocess.Popen(
-            ['wm_reader', '-m', 'lsb', '-q', '-d', dst_dir, filepath],
+            ['wm_reader', '-m', 'lsb', '-q', '-d', dst_dir, '-s', suffix, filepath],
         )
     except FileNotFoundError:
         raise SkipTest('binaries are not present (hint: run tests via tox)')
