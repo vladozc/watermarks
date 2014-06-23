d98f52cee2a98c7bac412c932dd849505bb14c8f
Vladimir Chovanec <vladimir.chovanec.zc@gmail.com>
2014-06-23 18:18:12 +0200

    add option --version

--- src/watermarks/bin/__init__.py
+++ src/watermarks/bin/__init__.py
@@ -2,12 +2,26 @@ import argparse
 import logging
 import sys
 
+from watermarks import __version__ as version
+
 
 class WMParser(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write('error: %s\n' % message)
         self.print_help()
         sys.exit(2)
+
+
+def handle_version():
+    if '--version' in sys.argv:
+        print(version)
+        sys.exit(0)
+
+
+def handle_help(parser):
+    if '-h' in sys.argv or '--help' in sys.argv:
+        parser.print_help()
+        sys.exit(0)
 
 
 def get_log_level(args):
@@ -20,12 +34,18 @@ def get_log_level(args):
     return ll
 
 
-def add_vq_args(parser):
+def add_basic_args(parser):
     parser.add_argument(
         '-q', '--quiet', action='store_true', help='Be quiet.'
     )
     parser.add_argument(
         '-v', '--verbose', action='store_true', help='Be verbose.'
+    )
+    parser.add_argument(
+        '--version', action='store_true', help='Show version and quit.'
+    )
+    parser.add_argument(
+        '-h', '--help', action='store_true', help='Show this help message and exit.'
     )
 
 
